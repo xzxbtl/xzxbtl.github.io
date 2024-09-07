955e30f7c151ea3b9811956177b6ed0760980b98
xzxbtl <157987211+xzxbtl@users.noreply.github.com>
2024-09-07 03:20:48 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -11,23 +11,35 @@ let isRed = false;
 image.onclick = function() {
     let currentScore = parseInt(scoreElement.textContent);
     currentScore += 1;
-    scoreElement.textContent = currentScore; 
+    scoreElement.textContent = currentScore;
 
     if (currentScore % 20 === 0 && !isRed) {
-    scoreElement.style.cssText = `
-        color: white;
-        animation: scaleScore100 1.5s ease-out; 
-    `;
-    isRed = true;
+        scoreElement.style.cssText = `
+            color: white;
+            animation: scaleScore100 1.5s ease-out; 
+        `;
+        isRed = true;
 
-    scoreElement.addEventListener('animationend', () => {
-        isRed = false;
-        scoreElement.style.removeProperty('animation');
-    });
+        scoreElement.addEventListener('animationend', () => {
+            isRed = false;
+            scoreElement.style.removeProperty('animation');
+        });
     }
+
+    const plusOneElement = document.createElement('span');
+    plusOneElement.textContent = '+1';
+    plusOneElement.classList.add('plus-one');
+
+    const position = Math.random() < 0.5 ? 'top' : 'bottom';
+    plusOneElement.classList.add(position);
+
+    document.querySelector('.coin-block').appendChild(plusOneElement);
+
+    plusOneElement.addEventListener('animationend', () => {
+        document.querySelector('.coin-block').removeChild(plusOneElement);
+    });
 };
 
-// Определение анимации scaleScore100
 const scaleScore100 = document.createElement('style');
 scaleScore100.innerHTML = `
 @keyframes scaleScore100 {
@@ -36,4 +48,4 @@ scaleScore100.innerHTML = `
     100% { transform: scale(1); color: white; } 
 }
 `;
-document.head.appendChild(scaleScore100); 
+document.head.appendChild(scaleScore100);
