bbb4ce32204dc8d6ee546b8b2a6cb17a053ed5ff
xzxbtl <157987211+xzxbtl@users.noreply.github.com>
2024-09-07 06:18:07 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -10,53 +10,57 @@ const image = document.getElementById("coin");
 let scoreElement = document.getElementById("score");
 let isRed = false;
 
-image.onclick = function() {
-    let currentScore = parseInt(scoreElement.textContent);
-    const randomPoints = Math.floor(Math.random() * 3) + 1;
-    currentScore += randomPoints;
-    scoreElement.textContent = currentScore;
-    tg.MainButton.show();
-
-
-    if (currentScore % 50 === 0 && !isRed) {
-        scoreElement.style.cssText = `
-            color: white;
-            animation: scaleScore100 1.5s ease-out; 
-        `;
-        isRed = true;
-
-        scoreElement.addEventListener('animationend', () => {
-            isRed = false;
-            scoreElement.style.removeProperty('animation');
+try {
+    image.onclick = function() {
+        let currentScore = parseInt(scoreElement.textContent);
+        const randomPoints = Math.floor(Math.random() * 3) + 1;
+        currentScore += randomPoints;
+        scoreElement.textContent = currentScore;
+        tg.MainButton.show();
+    
+    
+        if (currentScore % 50 === 0 && !isRed) {
+            scoreElement.style.cssText = `
+                color: white;
+                animation: scaleScore100 1.5s ease-out; 
+            `;
+            isRed = true;
+    
+            scoreElement.addEventListener('animationend', () => {
+                isRed = false;
+                scoreElement.style.removeProperty('animation');
+            });
+        }
+    
+        const plusOneElement = document.createElement('span');
+        plusOneElement.textContent = `+${randomPoints}`;
+        plusOneElement.classList.add('plus-one');
+    
+        const position = Math.random() < 0.5 ? 'top' : 'bottom';
+        plusOneElement.classList.add(position);
+    
+        document.querySelector('.coin-block').appendChild(plusOneElement);
+    
+        plusOneElement.addEventListener('animationend', () => {
+            document.querySelector('.coin-block').removeChild(plusOneElement);
         });
+    };
+    
+    const scaleScore100 = document.createElement('style');
+    scaleScore100.innerHTML = `
+    @keyframes scaleScore100 {
+        0% { transform: scale(1); color: red; } 
+        50% { transform: scale(1.2); color: red; } 
+        100% { transform: scale(1); color: white; } 
     }
-
-    const plusOneElement = document.createElement('span');
-    plusOneElement.textContent = `+${randomPoints}`;
-    plusOneElement.classList.add('plus-one');
-
-    const position = Math.random() < 0.5 ? 'top' : 'bottom';
-    plusOneElement.classList.add(position);
-
-    document.querySelector('.coin-block').appendChild(plusOneElement);
-
-    plusOneElement.addEventListener('animationend', () => {
-        document.querySelector('.coin-block').removeChild(plusOneElement);
+    `;
+    document.head.appendChild(scaleScore100);
+    
+    Telegram.WebApp.onEvent("mainButtonClicked", function(){
+        let score = parseInt(scoreElement.textContent);
+        tg.sendData(score.toString());
+        tg.close();
     });
-};
-
-const scaleScore100 = document.createElement('style');
-scaleScore100.innerHTML = `
-@keyframes scaleScore100 {
-    0% { transform: scale(1); color: red; } 
-    50% { transform: scale(1.2); color: red; } 
-    100% { transform: scale(1); color: white; } 
+} catch (error) {
+    tg.sendData("Ошибка: " + error.message);
 }
-`;
-document.head.appendChild(scaleScore100);
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    let score = parseInt(scoreElement.textContent);
-    tg.sendData(score.toString());
-    tg.close();
-});
